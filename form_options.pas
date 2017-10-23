6e9b64606f2db5629751493cd1d4245c37cb0470
Alexey-t <support@uvviewsoft.com>
2017-10-23 14:03:23 +0300

    fixed: x64, crash in Options dialog

--- form_options.pas
+++ form_options.pas
@@ -18,14 +18,21 @@ type
     btnColorBack: TButton;
     btnClose: TButton;
     ColorDialog1: TColorDialog;
-    comboNums: TComboBox;
     FontDialog1: TFontDialog;
     Label1: TLabel;
     labelFont: TLabel;
+    chkNumsAll: TRadioButton;
+    chkNumsNone: TRadioButton;
+    chkNums10: TRadioButton;
+    chkNums5: TRadioButton;
     procedure btnColorBackClick(Sender: TObject);
     procedure btnColorFontClick(Sender: TObject);
     procedure btnFontClick(Sender: TObject);
     procedure btnCloseClick(Sender: TObject);
+    procedure chkNums10Change(Sender: TObject);
+    procedure chkNums5Change(Sender: TObject);
+    procedure chkNumsAllChange(Sender: TObject);
+    procedure chkNumsNoneChange(Sender: TObject);
     procedure comboNumsChange(Sender: TObject);
     procedure FormShow(Sender: TObject);
   private
@@ -45,7 +52,12 @@ implementation
 procedure TfmOptions.FormShow(Sender: TObject);
 begin
   labelFont.Caption:= Format('%s, %d', [ed.Font.Name, ed.Font.Size]);
-  comboNums.ItemIndex:= Ord(ed.OptNumbersStyle);
+  case ed.OptNumbersStyle of
+    cNumbersAll: chkNumsAll.Checked:= true;
+    cNumbersNone: chkNumsNone.Checked:= true;
+    cNumbersEach10th: chkNums10.Checked:= true;
+    cNumbersEach5th: chkNums5.Checked:= true;
+  end;
 end;
 
 procedure TfmOptions.btnFontClick(Sender: TObject);
@@ -84,11 +96,35 @@ begin
   Close;
 end;
 
-procedure TfmOptions.comboNumsChange(Sender: TObject);
+procedure TfmOptions.chkNums10Change(Sender: TObject);
 begin
-  ed.OptNumbersStyle:= TATSynNumbersStyle(comboNums.ItemIndex);
+  ed.OptNumbersStyle:= cNumbersEach10th;
   ed.Update;
 end;
+
+procedure TfmOptions.chkNums5Change(Sender: TObject);
+begin
+  ed.OptNumbersStyle:= cNumbersEach5th;
+  ed.Update;
+end;
+
+procedure TfmOptions.chkNumsAllChange(Sender: TObject);
+begin
+  ed.OptNumbersStyle:= cNumbersAll;
+  ed.Update;
+end;
+
+procedure TfmOptions.chkNumsNoneChange(Sender: TObject);
+begin
+  ed.OptNumbersStyle:= cNumbersNone;
+  ed.Update;
+end;
+
+procedure TfmOptions.comboNumsChange(Sender: TObject);
+begin
+
+end;
+
 
 end.
 
